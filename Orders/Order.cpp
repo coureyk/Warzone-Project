8b3a3b209a569c0d2a7ccae425c611f969d9d60d
coureyk <kcourey49@gmail.com>
2024-09-30 20:58:39 -0400

    Included remaining Order subclasses with empty definitions for validate() and execute() and placeholder definitions for summary().

--- Orders/Order.cpp
+++ Orders/Order.cpp
@@ -1,6 +1,6 @@
 #include "Order.h"
 
-//Defining class members for Order
+//DEFINING CLASS MEMBERS FOR ORDER
 std::string Order::getOrderType() {
     return this->orderType;
 }
@@ -9,7 +9,7 @@ void Order::setOrderType(std::string orderType) {
     this->orderType = orderType;
 }
 
-//Defining class members for Deploy
+//DEFINING CLASS MEMBERS FOR DEPLOY
 Deploy::Deploy(int armyUnits, std::string targetTerritory) {
     this->armyUnits = armyUnits;
     this->targetTerritory = targetTerritory;
@@ -34,4 +34,132 @@ void Deploy::execute() {
 
 void Deploy::summary() {
     std::cout << "Army Units: " << getArmyUnits() << "\nTarget Territory: " << getTargetTerritory() << "\n";
+}
+
+//DEFINING CLASS MEMBERS FOR ADVANCE
+Advance::Advance(int armyUnits, std::string sourceTerritory, std::string targetTerritory) {
+    this->armyUnits = armyUnits;
+    this->sourceTerritory = sourceTerritory;
+    this->targetTerritory = targetTerritory;
+}
+
+int Advance::getArmyUnits() {
+    return this->armyUnits;
+}
+
+std::string Advance::getSourceTerritory() {
+    return this->sourceTerritory;
+}
+
+std::string Advance::getTargetTerritory() {
+    return this->targetTerritory;
+}
+
+bool Advance::validate() {
+    return true;
+}
+
+void Advance::execute() {
+
+}
+
+void Advance::summary() {
+    std::cout << "Army Units: " << getArmyUnits() << "\nSource Territory: " << getSourceTerritory() << "\nTarget Territory : " << getTargetTerritory() << "\n";
+}
+
+//DEFINING CLASS MEMBERS FOR BOMB
+Bomb::Bomb(std::string targetTerritory) {
+    this->targetTerritory = targetTerritory;
+}
+
+std::string Bomb::getTargetTerritory() {
+    return this->targetTerritory;
+}
+
+bool Bomb::validate() {
+    return true;
+}
+
+void Bomb::execute() {
+
+}
+
+void Bomb::summary() {
+    std::cout << "Target Territory : " << getTargetTerritory() << "\n";
+}
+
+//DEFINING CLASS MEMBERS FOR BLOCKADE
+Blockade::Blockade(std::string targetTerritory) {
+    this->targetTerritory = targetTerritory;
+}
+
+std::string Blockade::getTargetTerritory() {
+    return this->targetTerritory;
+}
+
+bool Blockade::validate() {
+    return true;
+}
+
+void Blockade::execute() {
+
+}
+
+void Blockade::summary() {
+    std::cout << "Target Territory : " << getTargetTerritory() << "\n";
+}
+
+Airlift::Airlift(int armyUnits, std::string sourceTerritory, std::string targetTerritory) {
+    this->armyUnits = armyUnits;
+    this->sourceTerritory = sourceTerritory;
+    this->targetTerritory = targetTerritory;
+}
+
+int Airlift::getArmyUnits() {
+    return this->armyUnits;
+}
+
+std::string Airlift::getSourceTerritory() {
+    return this->sourceTerritory;
+}
+
+std::string Airlift::getTargetTerritory() {
+    return this->targetTerritory;
+}
+
+bool Airlift::validate() {
+    return true;
+}
+
+void Airlift::execute() {
+
+}
+
+void Airlift::summary() {
+    std::cout << "Army Units: " << getArmyUnits() << "\nSource Territory: " << getSourceTerritory() << "\nTarget Territory : " << getTargetTerritory() << "\n";
+}
+
+Negotiate::Negotiate(std::string sourcePlayer, std::string targetPlayer) {
+    this->sourcePlayer = sourcePlayer;
+    this->targetPlayer = targetPlayer;
+}
+
+std::string Negotiate::getSourcePlayer() {
+    return this->sourcePlayer;
+}
+
+std::string Negotiate::getTargetPlayer() {
+    return this->targetPlayer;
+}
+
+bool Negotiate::validate() {
+    return true;
+}
+
+void Negotiate::execute() {
+
+}
+
+void Negotiate::summary() {
+    std::cout << "Source Player: " << getSourcePlayer() << "\nTarget Player: " << getTargetPlayer() << "\n";
 }
--- Orders/Order.h
+++ Orders/Order.h
@@ -32,4 +32,86 @@ public:
     void summary();
 };
 
+class Advance : public Order {
+private:
+    int armyUnits;
+    std::string sourceTerritory;
+    std::string targetTerritory;
+
+public:
+    Advance(int armyUnits, std::string sourceTerritory, std::string targetTerritory);
+
+    int getArmyUnits();
+    std::string getSourceTerritory();
+    std::string getTargetTerritory();
+
+    bool validate();
+    void execute();
+    void summary();
+};
+
+class Bomb : public Order {
+private:
+    std::string targetTerritory;
+
+public:
+    Bomb(std::string targetTerritory);
+
+    std::string getTargetTerritory();
+
+    bool validate();
+    void execute();
+    void summary();
+};
+
+class Blockade : public Order {
+private:
+    std::string targetTerritory;
+
+public:
+    Blockade(std::string targetTerritory);
+
+    std::string getTargetTerritory();
+
+    bool validate();
+    void execute();
+    void summary();
+};
+
+class Airlift : public Order {
+private:
+    int armyUnits;
+    std::string sourceTerritory;
+    std::string targetTerritory;
+
+public:
+    Airlift(int armyUnits, std::string sourceTerritory, std::string targetTerritory);
+
+    int getArmyUnits();
+    std::string getSourceTerritory();
+    std::string getTargetTerritory();
+
+    bool validate();
+    void execute();
+    void summary();
+};
+
+class Negotiate : public Order {
+private:
+    std::string sourcePlayer;
+    std::string targetPlayer;
+
+public:
+    Negotiate(std::string sourcePlayer, std::string targetPlayer);
+
+    std::string getSourcePlayer();
+    std::string getTargetPlayer();
+
+    bool validate();
+    void execute();
+    void summary();
+};
+
+
+
 #endif
