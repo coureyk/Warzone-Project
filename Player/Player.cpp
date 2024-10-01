2dd761d625509897b1cad2c010ac35f8ac8452fd
ThickShligga <Liam_Di_Francesco@hotmail.com>
2024-10-01 19:24:52 -0400

    8=======================>

---
+++ Player/Player.cpp
@@ -0,0 +1,48 @@
+﻿#include "Player.h"
+#include <iostream>
+
+
+Player::Player(std::vector<Territory>*territories,OrdersList* ordersList, Hand* hand) {
+
+	this->territories = territories;
+
+	this->ordersList = ordersList;
+
+	this->hand = hand;
+
+}
+
+Player::Player() {
+
+	territories = new std::vector<Territory>;
+
+	ordersList = new OrdersList;
+
+	hand = new Hand;
+
+}
+
+std::vector<Territory>* Player::toDefend() {
+
+
+
+}
+
+std::vector<Territory>* Player::toAttack() {
+
+
+
+}
+
+void Player::issueOrder() {
+
+	std::cout << "Which order would you like to issue?";
+
+	Deploy* deploy = new Deploy;
+
+	ordersList->addLast(deploy);
+
+
+
+
+}
