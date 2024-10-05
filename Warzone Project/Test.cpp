7a6d77c87972440defbe127a589efefde3dbd0d5
coureyk <kcourey49@gmail.com>
2024-10-05 01:28:25 -0400

    Modified references in Warzone Project within Map branch and modified Test.cpp

--- Warzone Project/Test.cpp
+++ Warzone Project/Test.cpp
@@ -1,31 +1,5 @@
-#include "Player.h"
-#include <vector>
+#include <mapDriver.cpp>
 
 int main() {
-
-	std::vector<Territory>* territories = new std::vector<Territory>;
-
-	std::string s = "";
-
-	Territory* territory1 = new Territory(s);
-
-	territories->push_back(*territory1);
-	
-	Card* card1 = new Card(BOMB);
-
-	Hand* hand1 = new Hand;
-
-	hand1->addCard(card1);
-
-	OrdersList* list = new OrdersList;
-
-	Deploy* order1 = new Deploy(1,std::string("Alaska"));
-
-	list->addLast(order1);
-
-	Player* player1 = new Player(*territories, *list, *hand1);
-
-	player1->issueOrder();
-
-	
+	testLoadMaps();
 }
