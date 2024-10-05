be3899d62a596dff22d769994449068129eddbff
coureyk <kcourey49@gmail.com>
2024-10-05 03:29:32 -0400

    update2

--- Warzone Project/Test.cpp
+++ Warzone Project/Test.cpp
@@ -1,8 +1,14 @@
 #include "Cards.h"
 #include "CardsDriver.cpp"
+#include "GameEngine.h"
 
 
 int main() {
-    std::cout << "TESTING CARDS METHODS\n\n"
+    std::cout << "TESTING CARDS METHODS\n\n\n";
     testCards();
+
+    std::cout << "\n\n\nTESTING GAME_ENGINE METHODS";
+    testGameStates();
+
+
 }
