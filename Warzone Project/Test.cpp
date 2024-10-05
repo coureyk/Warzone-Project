658524d9ecfd37ecaa2b327cddb927780d3ccadd
coureyk <kcourey49@gmail.com>
2024-10-05 12:04:02 -0400

    player commit

--- Warzone Project/Test.cpp
+++ Warzone Project/Test.cpp
@@ -3,9 +3,11 @@
 
 #include "GameEngine.h"
 
-#include "MapDriver.cpp"
-
 #include "OrdersDriver.h"
+
+#include "Player.h"
+
+#include "MapDriver.cpp"
 
 int main() {
     std::cout << "testCards() is invoked\n\n\n";
@@ -16,6 +18,9 @@ int main() {
 
     std::cout << "\n\n\ntestOrdersLists() is invoked\n\n\n";
     testOrdersLists();
+
+    std::cout << "\n\n\ntestPlayers() is invoked\n\n\n";
+    testPlayers();
 
     /* File reading causes error
     std::cout << "\n\n\ntestLoadMaps() is invoked";
