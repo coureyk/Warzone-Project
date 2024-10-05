ac3db87f9917b7c09cd6e5c5e70994ec53e3c302
coureyk <kcourey49@gmail.com>
2024-10-05 11:23:53 -0400

    gameengine commit

--- Warzone Project/Test.cpp
+++ Warzone Project/Test.cpp
@@ -1,8 +1,12 @@
-
 #include "Cards.h"
 #include "CardsDriver.cpp"
 
+#include "GameEngine.h"
 
 int main() {
+    std::cout << "testCards() is invoked\n\n\n";
     testCards();
+
+    std::cout << "\n\n\ntestGameStates() is invoked\n\n\n";
+    testGameStates();
 }
