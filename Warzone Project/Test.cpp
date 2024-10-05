93a2d73c21d06fd61a65f39a0354fcd9a6f05a18
coureyk <kcourey49@gmail.com>
2024-10-05 11:37:39 -0400

    map commit

--- Warzone Project/Test.cpp
+++ Warzone Project/Test.cpp
@@ -3,10 +3,17 @@
 
 #include "GameEngine.h"
 
+#include "MapDriver.cpp""
+
 int main() {
     std::cout << "testCards() is invoked\n\n\n";
     testCards();
 
     std::cout << "\n\n\ntestGameStates() is invoked\n\n\n";
     testGameStates();
+
+    /* File reading causes error
+    std::cout << "\n\n\ntestLoadMaps() is invoked";
+    testLoadMaps();
+    */
 }
